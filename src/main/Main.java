f436fea993b12a442a2e750fe1be3beef42b034a
DavidGL17 <david.gonzalezleon@epfl.ch>
2016-10-24 10:43:51 +0200

    Whala

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+/**
+ * @author
+ */
+public final class Main {
+
+    public static void main(String[] args) {
+    	//pour séparer chaque couleur
+    	int image[][] = Helper.read("images/AdeleBlochBauer/AdeleBlochBauer.png");
+    	Helper.show(image, "AdeleBlochBauer");
+    	
+    	for (int i=0;i<image.length;++i){
+    		for (int j=0;j<image[i].length;++j){
+    			int alpha = ((image[i][j] & 0xff000000 ) >> 24);
+    			int red = ((image[i][j] & 0x00ff0000 ) >> 16);
+    			int green = ((image[i][j] & 0x0000ff00 ) >> 8);
+    			int blue = ((image[i][j] & 0x000000ff ));
+    			System.out.printf("image[%d][%d]=(%d, %d, %d, %d)\n",i,j,alpha,red,green,blue);
+    		}
+    		System.out.println();
+    	}
+    }
+
+}
