f9109b43a3447a33cecf85f2271b911cfa2d0cfc
TozeMarques <toze_maluko@hotmail.com>
2016-10-28 13:37:12 +0200

    image hd

--- src/main/MainAntonio.java
+++ src/main/MainAntonio.java
@@ -5,23 +5,8 @@ public final class MainAntonio {
 	public static void main(String[] args) {
 		
 	
-	boolean[] bitArray = new boolean[1];
-	bitArray[1] = false;
-    
-		String binaryString = "";
-    	int convert = 0;
-    	char c = 0;
-    	boolean[] binaryTab = new boolean [10];
-    	for (int j = 0 ; j < bitArray.length; j += 8){		
-    			for (int i = 0; i < 8; ++i){
-    			binaryTab[i] = bitArray[j + i];
-    			convert = TextMessage.bitArrayToInt(binaryTab);
-    			c = (char)convert;
-    			binaryString += c;
-    			System.out.println(binaryString);
-    			
-    			}
-    	
-	}
-	}
+	String path = "Hello";
+	
+	System.out.println(Helper.read(path));
+}
 }
